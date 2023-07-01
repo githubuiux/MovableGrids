799e6d1a8f314e12f433000e31662d337370a22c
githubuiux <uiuxista@gmail.com>
2023-07-01 11:03:36 +0300

    Initial Commit

---
+++ MoveableGrids/MoveableGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveableGridsApp.swift
+//  MoveableGrids
+//
+//  Created by Levent on 1.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoveableGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
